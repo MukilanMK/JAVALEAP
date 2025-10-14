652a552af6dbf749dc1891fd0d622dd4f353d248
Mukilan Muthukumar K <150666356+MukilanMK@users.noreply.github.com>
2025-10-14 12:35:27 +0530

    First commit

---
+++ Day2/src/main/java/com/example/studentcurd/repository/StudentRepository.java
@@ -0,0 +1,8 @@
+package com.example.studentcurd.repository;
+import com.example.studentcurd.entity.Student;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+}
